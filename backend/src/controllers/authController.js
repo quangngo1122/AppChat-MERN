9466f4df4009c796521da8fc0aa24d43c57605c9
quangngo1122 <mrquang9a1apt@gmail.com>
2026-02-18 10:24:24 +0700

    feat(auth): Tạo api đăng nhập signIn với: - xác thực thông tin đăng nhập bằng bcrypt - tạo JWT access token - tạo session refresh token trong DB - gửi refresh token qua cookie httpOnly

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
-import User from "../models/User";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import Session from "../models/Session.js";
+import crypto from "crypto";
+
+const ACCESS_TOKEN_TTL = "30m"; // thường dưới 15', o localhost này làm 30' cho dể test
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // 14 ngay
 
 export const signUp = async (req, res) => {
   try {
@@ -19,7 +25,6 @@ export const signUp = async (req, res) => {
     }
     // nếu chưa --> mã hóa pass
     const hashedPassword = await bcrypt.hash(password, 10); // ma hoa pass goc 2^10 lan, thuong dung 10 hoac 12
-
     // tạo user mới
     await User.create({
       username,
@@ -31,6 +36,63 @@ export const signUp = async (req, res) => {
     return res.sendStatus(204);
   } catch (error) {
     console.log("loi khi goi signUp", error);
+    return res.status(500).json({ message: "loi he thong" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    // lay Input dc gui len tu client qua req.body
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({
+        message: "Thiếu username hoac password",
+      });
+    }
+    // lấy hashedPassword trong db so sánh với pass Input
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "username hoặc password không chính xác",
+      });
+    }
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword); // so sánh 2 pass có trùng nhau ko
+    if (!passwordCorrect) {
+      return res.status(401).json({
+        message: "username hoặc password không chính xác",
+      });
+    }
+    // nếu khớp, tạo accesstoken = JWT
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: ACCESS_TOKEN_TTL },
+    ); // payload - secrect key - option
+
+    // tạo refresh token
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    // tạo session trong db mới để lưu refresh token
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+    // gửi refresh token về client qua trong cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true, // không thể bị truy cập bởi js
+      secure: true, // đảm bảo chỉ gửi qua https
+      sameSite: "none", // cho phép backend, frontend chạy trên 2 domain khác nhau
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    // trả access token về trong res
+    return res
+      .status(200)
+      .json({ message: `User ${user.displayName} da login`, accessToken });
+  } catch (error) {
+    console.log("loi khi goi signIp", error);
     return res.status(500).json({ message: "loi he thong" });
   }
 };
