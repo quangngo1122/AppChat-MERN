7379181de287c9b60c0a908397edab315c13dfce
quangngo1122 <mrquang9a1apt@gmail.com>
2026-03-30 20:16:04 +0700

    feat(deploy): thêm file vercel.json để giải quyết tình trạng ko load được trang khi thay đổi đường dẫn bằng cách nhập trên thanh tìm kiếm thay vì chọn nút navigate qua đường dẫn đó - Thêm logic không vào trang đăng nhập đăng ký khi đã đăng nhập rồi

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,6 +1,23 @@
 import { SigninForm } from "@/components/auth/signin-form";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 
 const SignInPage = () => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  const hasShownToast = useRef(false);
+
+  useEffect(() => {
+    if (user && !hasShownToast.current) {
+      hasShownToast.current = true;
+      toast.info("Bạn đã đăng nhập");
+      navigate("/", { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 absolute inset-0 z-0 bg-gradient-purple">
       <div className="w-full max-w-sm md:max-w-4xl">
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,6 +1,22 @@
 import { SignupForm } from "@/components/auth/signup-form";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 
 const SignUpPage = () => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+  const hasShownToast = useRef(false);
+
+  useEffect(() => {
+    if (user && !hasShownToast.current) {
+      hasShownToast.current = true;
+      toast.info("Hãy đăng xuất trước");
+      navigate("/", { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 absolute inset-0 z-0 bg-gradient-purple">
       <div className="w-full max-w-sm md:max-w-4xl">
